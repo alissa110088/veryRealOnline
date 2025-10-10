c93cb97a120fa950230a98510b8c41988c31f0ce
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-10-10 10:58:43 +0200

    Start of camera controller

---
+++ VeryRealOnline/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private const string xAxis = "Mouse X";
+    private const string yAxis = "Mouse Y";
+
+    private Vector2 rotation = Vector2.zero;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rotation.x += Input.GetAxis(xAxis) * 5f;
+        rotation.y += Input.GetAxis(yAxis) * 5f;
+        rotation.y = Mathf.Clamp(rotation.y, -88f, 88f);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotation.x, Vector2.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotation.y, Vector2.left);
+
+        transform.localRotation = xQuaternion * yQuaternion;
+    }
+}
