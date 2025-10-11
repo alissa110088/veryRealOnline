7a4aec51fe435eb4919cd300f1bca51378a11448
alissa110088 <delattre.alissa@gmail.com>
2025-10-12 01:31:42 +0200

    jump

--- VeryRealOnline/Assets/Scripts/PlayerNetwork.cs
+++ VeryRealOnline/Assets/Scripts/PlayerNetwork.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Collections;
 using Unity.Netcode;
 using Unity.VisualScripting;
@@ -8,10 +9,17 @@ public class PlayerNetwork : NetworkBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private float airControlMultiplier = 0.3f;
+    [SerializeField] private float jumpForce = 5f;
 
     private Vector3 direction;
     private InputSystem_Actions inputActions;
     private Vector3 inputDirection;
+
+    private bool isGrounded;
+
+    private const string groundLayerName = "Ground";
+    private const string ObstacleLayerName = "Obstacle";
 
     public override void OnNetworkSpawn()
     {
@@ -20,8 +28,11 @@ public class PlayerNetwork : NetworkBehaviour
         inputActions = new InputSystem_Actions();
         inputActions.Player.Move.performed += GetDirection;
         inputActions.Player.Move.canceled += ctx =>  inputDirection = Vector3.zero;
+        inputActions.Player.Jump.performed += Jump;
         inputActions.Player.Enable();
+
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public override void OnNetworkDespawn()
@@ -35,11 +46,58 @@ public class PlayerNetwork : NetworkBehaviour
     {
         if (!IsOwner) return;
 
-        direction = transform.forward * inputDirection.z + transform.right * inputDirection.x;
-        rb.AddForce(direction * moveSpeed);
-        rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, 10f);
         Debug.Log(rb.linearVelocity);
 
+        direction = transform.forward * inputDirection.z + transform.right * inputDirection.x;
+
+        //TODO ne marche pas a regler
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction.normalized,out hit, 1f))
+        {
+            Vector3 push = Vector3.Cross(hit.normal, Vector3.up);
+            rb.position += push * 0.01f;
+        }
+
+        if (isGrounded)
+        {
+            direction = direction * moveSpeed;
+            rb.linearVelocity = new Vector3(direction.x, rb.linearVelocity.y, direction.z);
+        }
+        else
+        {
+            direction = direction * moveSpeed * .7f;
+            rb.linearVelocity = new Vector3(
+                Mathf.Lerp(rb.linearVelocity.x, direction.x, Time.fixedDeltaTime * 5f),
+                rb.linearVelocity.y,
+                Mathf.Lerp(rb.linearVelocity.z, direction.z, Time.fixedDeltaTime * 5f)
+            );
+
+        }
+
+        Vector3 horizontal = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        horizontal = Vector3.ClampMagnitude(horizontal, 8f);
+        rb.linearVelocity = new Vector3(horizontal.x, rb.linearVelocity.y, horizontal.z);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!IsOwner) return;
+
+        if(collision.gameObject.layer == LayerMask.NameToLayer(groundLayerName) && !isGrounded)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(!IsOwner) return;
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer(groundLayerName) && isGrounded)
+        {
+            isGrounded = false;
+        }
     }
 
     private void GetDirection(InputAction.CallbackContext ctx)
@@ -51,4 +109,15 @@ public class PlayerNetwork : NetworkBehaviour
         inputDirection = new Vector3(inputDirection.x, 0f, inputDirection.y);
         inputDirection = inputDirection.normalized;
     }
+
+    private void Jump(InputAction.CallbackContext ctx)
+    {
+        if(!IsOwner) return;
+        if (isGrounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
 }
