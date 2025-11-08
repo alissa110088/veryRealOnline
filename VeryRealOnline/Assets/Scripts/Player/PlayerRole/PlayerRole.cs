9470dff5ef4882f63824c1028835c669c6d459fe
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-11-08 20:13:01 +0100

    text indication

--- VeryRealOnline/Assets/Scripts/Player/PlayerRole/PlayerRole.cs
+++ VeryRealOnline/Assets/Scripts/Player/PlayerRole/PlayerRole.cs
@@ -1,12 +1,17 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerRole : MonoBehaviour
 {
     private EnumPlayerState playerRole;
+    private string hiderText = "hider";
+    private string seekerText = "seeker";
+
 
     [SerializeField] private Hider hiderScript;
     [SerializeField] private Seeker seekerScript;
+    [SerializeField] private TextMeshProUGUI text;
 
     private void OnEnable()
     {
@@ -30,12 +35,16 @@ public class PlayerRole : MonoBehaviour
         if(playerRole == EnumPlayerState.seeker)
         {
             seekerScript.enabled = true;
-            gameObject.tag = "seeker";
+            gameObject.tag = seekerText;
+            text.text = seekerText;
+            text.color = Color.red;
         }
         else if(playerRole == EnumPlayerState.hider)
         {
             hiderScript.enabled = true;
-            gameObject.tag = "hider";
+            gameObject.tag = hiderText;
+            text.text = hiderText;
+            text.color = Color.green;
         }
     }
 }
